acc7ff7c54fcc4b8d0f9c62627371fc7f7cf50df
Howard <howard.yu.96@gmail.com>
2017-05-28 00:34:23 -0700

    Added constants for urls

---
+++ app/src/main/java/com/cs122b/group41/fabflix/Constants/URLContants.java
@@ -0,0 +1,11 @@
+package com.cs122b.group41.fabflix.Constants;
+
+/**
+ * Created by howard on 5/27/17.
+ */
+
+public class URLContants {
+    public static final String BASE_DEV_SERVER_URL = "http://192.168.29.100:8080";
+    public static final String BASE_PROD_SERVER_URL = "http://aws.howardyu.net:8080/fabflix";
+
+}
